87d9a1a7334743f37685a1bdd02c4250c7ffcc62
Jeongyeon Park <64348346+jeongyeon-park@users.noreply.github.com>
2022-11-10 11:13:21 -0600

    MOD: 반응형 헤더 구현중

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { useNavigate, useParams } from "react-router-dom";
+import { FaHome, FaBars } from "react-icons/fa";
 
 const Header = () => {
 
     const navigate = useNavigate();
 
-
+    const toggle = useRef();
 
 
     const goNotice = (e) => {
@@ -25,14 +26,30 @@ const Header = () => {
         navigate("/login");
     }
 
+    const onToggle = () => {
+        toggle.current.classList.toggle('active');
+        console.log(toggle.current);
+    }
     return (
         <div className='Header'>
-            <div onClick={goHome}>메인</div>
-            <div className='sub-menu'>
-                <div onClick={goNotice}>공지사항</div>
-                <div >위치서비스</div>
-                <div onClick={goQuesiton}>문의</div>
+            <div className="left-menu" onClick={goHome}>
+                <FaHome className='home-icon'></FaHome>
+            </div>
+
+            <ul className='middle-menu'>
+                <li >위치서비스</li>
+                <li onClick={goNotice}>공지사항</li>
+                <li onClick={goQuesiton}>문의</li>
+                <li>최근 검색 목록</li>
+                <li>요기 모여요</li>
+            </ul>
+
+            <div className='right-menu'>
                 <div onClick={goLogin}>로그인</div>
+            </div>
+
+            <div className='toggleBtn' ref={toggle} onClick={onToggle}>
+                <FaBars ></FaBars>
             </div>
         </div>
     );
