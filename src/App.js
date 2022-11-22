2a52c4867d07225cc8688ae87b00c04cea875e0b
jeongyeon <dus7653@gmail.com>
2022-11-22 14:54:29 -0600

    ADD: nickname Context API 설정

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import GrabAuth from './components/GrabAuth';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import NoticeContent from './components/NoticeContent';
 
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { API } from './util/api';
 import Login from './pages/Login';
@@ -22,6 +22,7 @@ import SearchCommunity from './pages/SearchCommunity';
 import Room from './pages/Room';
 
 export const NoticeListContext = React.createContext();
+export const NickNameContext = React.createContext();
 
 function App() {
 
@@ -33,32 +34,66 @@ function App() {
     ).then(data => setNoticeList(data.data));
   }
 
-  useEffect(() => { getNoticeList() }, []);
+  const [jwtToken, setJwtToken] = useState(sessionStorage.getItem('jwtToken'));
+  const [userInfo, setUserInfo] = useState({ "nickname": "", "email": "" });
+
+  const postToken = async () => {
+    try {
+      let res = await fetch(`${API}/user/token`, {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ token: jwtToken })
+      }).then((res) => res.json())
+        .then(res => {
+          if (res.statusCode == 200) {
+            setUserInfo(res.data);
+          }
+        })
+    }
+    catch (e) {
+      console.log(e);
+    }
+  }
+
+  useEffect(() => {
+    getNoticeList();
+    setJwtToken(sessionStorage.getItem('jwtToken'));
+  }, []);
+
+  useEffect(() => {
+    if (jwtToken) {
+      postToken()
+    }
+  }, [])
 
   return (
     <NoticeListContext.Provider value={noticeList}>
-      <BrowserRouter>
-        <div className='App'>
-          <Header />
-          <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/oauth/kakao/callback" element={<GrabAuth />} />
-            <Route path="/oauth/naver/callback" element={<GrabAuthNaver />} />
-            <Route path='/oauth/nickname/:email' element={<LoginEmailInput />} />
-            <Route path="/info/:id" element={<Info />}></Route>
-            <Route path="/yogimoyo" exac element={<Community />}></Route>
-            <Route path='/yogimoyo/search' element={<SearchCommunity />}></Route>
-            <Route path='/yogimoyo/room/:rum' element={<Room />}></Route>
-            <Route path="/notice" exact element={<Notice />}></Route>
-            <Route path="/notice/:id" element={<NoticeContent />}></Route>
-            <Route path="/question" element={<BulletinBoard />}></Route>
-            <Route path="/login" element={<Login />}></Route>
-            <Route path="/map" element={<Map />}></Route>
-            <Route path="/userLogin" element={<UserLogin />}></Route>
-            {/* <Route path="/adminLogin" element={<AdminLogin />}></Route> */}
-          </Routes>
-        </div>
-      </BrowserRouter>
+      <NickNameContext.Provider value={userInfo}>
+        <BrowserRouter>
+          <div className='App'>
+            <Header />
+            <Routes>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="/oauth/kakao/callback" element={<GrabAuth />} />
+              <Route path="/oauth/naver/callback" element={<GrabAuthNaver />} />
+              <Route path='/oauth/nickname/:email' element={<LoginEmailInput />} />
+              <Route path="/info/:id" element={<Info />}></Route>
+              <Route path="/yogimoyo" exac element={<Community />}></Route>
+              <Route path='/yogimoyo/search' element={<SearchCommunity />}></Route>
+              <Route path='/yogimoyo/room/:rum' element={<Room />}></Route>
+              <Route path="/notice" exact element={<Notice />}></Route>
+              <Route path="/notice/:id" element={<NoticeContent />}></Route>
+              <Route path="/question" element={<BulletinBoard />}></Route>
+              <Route path="/login" element={<Login />}></Route>
+              <Route path="/map" element={<Map />}></Route>
+              <Route path="/userLogin" element={<UserLogin />}></Route>
+              {/* <Route path="/adminLogin" element={<AdminLogin />}></Route> */}
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </NickNameContext.Provider>
     </NoticeListContext.Provider>
 
   );
